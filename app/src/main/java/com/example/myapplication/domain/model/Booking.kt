mohamed-derardja/zm-rental-car

Add complete API integration framework with models, repositories, and documentation

app/src/main/java/com/example/myapplication/domain/model/Booking.kt
@@ -1,0 +1,35 @@
+package com.example.myapplication.domain.model
+
+import java.io.Serializable
+import java.util.Date
+
+data class Booking(
+    val id: String,
+    val userId: String,
+    val carId: String,
+    val startDate: Date,
+    val endDate: Date,
+    val totalPrice: Double,
+    val status: BookingStatus,
+    val paymentStatus: PaymentStatus,
+    val createdAt: Date,
+    val updatedAt: Date,
+    // These properties can be populated by the app after fetching the booking
+    var car: Car? = null,
+    var user: User? = null
+) : Serializable
+
+enum class BookingStatus {
+    PENDING,
+    CONFIRMED,
+    ACTIVE,
+    COMPLETED,
+    CANCELLED
+}
+
+enum class PaymentStatus {
+    PENDING,
+    PAID,
+    REFUNDED,
+    FAILED
+} 

app/src/main/java/com/example/myapplication/domain/model/Car.kt
@@ -1,0 +1,23 @@
+package com.example.myapplication.domain.model
+
+import java.io.Serializable
+
+data class Car(
+    val id: String,
+    val name: String,
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val category: String,
+    val imageUrl: String,
+    val rating: Double,
+    val transmission: String,
+    val fuel: String,
+    val seats: Int,
+    val price: Double,
+    val priceUnit: String = "DA/day",
+    val description: String,
+    val availability: Boolean = true,
+    val features: List<String> = emptyList(),
+    val location: String
+) : Serializable 

Zinou-web/Rental-car-app-Final-form

app/src/main/java/com/example/myapplication/domain/model/Booking.kt
@@ -1,0 +1,35 @@
+package com.example.myapplication.domain.model
+
+import java.io.Serializable
+import java.util.Date
+
+data class Booking1(
+    val id: String,
+    val userId: String,
+    val carId: String,
+    val startDate: Date,
+    val endDate: Date,
+    val totalPrice: Double,
+    val status: BookingStatus,
+    val paymentStatus: PaymentStatus,
+    val createdAt: Date,
+    val updatedAt: Date,
+    // These properties can be populated by the app after fetching the booking
+    var car: Car? = null,
+    var user: User? = null
+) : Serializable
+
+enum class BookingStatus1 {
+    PENDING,
+    CONFIRMED,
+    ACTIVE,
+    COMPLETED,
+    CANCELLED
+}
+
+enum class PaymentStatus {
+    PENDING,
+    PAID,
+    REFUNDED,
+    FAILED
+} 

app/src/main/java/com/example/myapplication/domain/model/Car.kt
@@ -1,0 +1,23 @@
+package com.example.myapplication.domain.model
+
+import java.io.Serializable
+
+data class Car1(
+    val id: String,
+    val name: String,
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val category: String,
+    val imageUrl: String,
+    val rating: Double,
+    val transmission: String,
+    val fuel: String,
+    val seats: Int,
+    val price: Double,
+    val priceUnit: String = "DA/day",
+    val description: String,
+    val availability: Boolean = true,
+    val features: List<String> = emptyList(),
+    val location: String
+) : Serializable 